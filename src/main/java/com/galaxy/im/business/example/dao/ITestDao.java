33aa2e768d6f4f4ff99c8455c584c3fabca02040
liyanqiao <qyz91888@163.com>
2017-05-23 18:50:27 +0800

    删除一些示例代码

--- src/main/java/com/galaxy/im/business/example/dao/ITestDao.java
+++
@@ -1,8 +0,0 @@
-package com.galaxy.im.business.example.dao;
-
-import com.galaxy.im.bean.Test;
-import com.galaxy.im.common.db.IBaseDao;
-
-public interface ITestDao extends IBaseDao<Test,Long>{
-
-}
