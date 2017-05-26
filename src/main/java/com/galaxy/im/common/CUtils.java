12a90898e3599961215c596ce015361ba3436697
liyanqiao <qyz91888@163.com>
2017-05-26 09:33:45 +0800

    去掉一些不应该出现的警告

--- src/main/java/com/galaxy/im/common/CUtils.java
+++ src/main/java/com/galaxy/im/common/CUtils.java
@@ -15,7 +15,6 @@ import org.apache.commons.beanutils.BeanUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
