36655b3817e3b485dfabcd80824c504a6228af09
liuli <liuli521>
2017-05-26 14:45:43 +0800

    修改日程冲突bug

--- src/main/java/com/galaxy/im/business/schedule/service/ScheduleServiceImpl.java
+++ src/main/java/com/galaxy/im/business/schedule/service/ScheduleServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.galaxy.im.bean.schedule.ScheduleInfo;
 import com.galaxy.im.business.schedule.dao.IScheduleDao;
+import com.galaxy.im.common.CUtils;
 import com.galaxy.im.common.db.IBaseDao;
 import com.galaxy.im.common.db.service.BaseServiceImpl;
 import com.galaxy.im.common.exception.ServiceException;
@@ -33,6 +34,29 @@ public class ScheduleServiceImpl extends BaseServiceImpl<ScheduleInfo> implement
 	@Override
 	public List<Map<String, Object>> ctSchedule(Map<String, Object> map) {
 		try{
+			String bqEndTime = "";
+			String eqStartTime ="";
+			long isAllday = 0;
+			if(map!=null && map.containsKey("endTime")){
+				bqEndTime = CUtils.get().object2String(map.get("endTime"), "");
+			}
+			if(map!=null && map.containsKey("startTime")){
+				eqStartTime = CUtils.get().object2String(map.get("startTime"), "");
+			}
+			if(map!=null && map.containsKey("isAllday")){
+				isAllday = CUtils.get().object2Long(map.get("isAllday"), 0L);
+			}
+			
+			if("".equals(bqEndTime) ||isAllday == 1){
+				bqEndTime = eqStartTime + " 23:59";
+			}
+
+			map.put("bqEndTime", bqEndTime);
+			map.put("eqStartTime", eqStartTime);
+			map.put("sbTimeForAllday", eqStartTime.substring(0, 10));
+			map.put("seTimeForAllday", bqEndTime.substring(0, 10));
+			map.put("isDel",0);
+			
 			List<Map<String, Object>> list = dao.getCTSchedule(map);
 			return list;
 		}catch(Exception e){
