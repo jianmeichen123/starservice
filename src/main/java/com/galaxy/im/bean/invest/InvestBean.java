f93954296e4cda8274eb855da0e16b06c28195ac
xiaochuang <chuangxiao@galaxyinternet.com>
2017-06-06 17:43:53 +0800

    修改投资方接口的实体类和数据库表信息

--- src/main/java/com/galaxy/im/bean/invest/InvestBean.java
+++ src/main/java/com/galaxy/im/bean/invest/InvestBean.java
@@ -8,100 +8,53 @@ public class InvestBean extends BaseEntity {
 	 * 投资方
 	 */
 	private static final long serialVersionUID = 2692500358849192889L;
-	private long personId;								//关联人力资源id
-	private String icompanyName;						//公司名称
-	private Double investmentAmount;					//投资金额
-	private Double shareRatio;							//股权占比
-	private String projectDirector;						//项目负责人
-	private String emceedPosition;						//担任职位
-	private String telephone;							//联系方式
-	private String acompanyName;						//a轮公司名称
-	private Double ainvestmentAmount;					//a轮投资金额
-	private Double ashareRatio;							//a轮股权占比
-	private String aemceedPosition;						//a轮担任职位
-	private String aprojectDirector;					//a轮项目负责人
-	private String atelephone;							//a轮联系方式;
-	public long getPersonId() {
-		return personId;
+	private int deliveryType;						//0-独投;1-领投;2-合投
+	private String deliveryName;					//合投机构名称
+	private Double deliveryAmount;					//投资金额
+	private Double deliveryShareRatio;			//股权占比
+	private int projectId;							//项目ID
+	private int createUid;							//创建人
+	public int getDeliveryType() {
+		return deliveryType;
 	}
-	public void setPersonId(long personId) {
-		this.personId = personId;
+	public void setDeliveryType(int deliveryType) {
+		this.deliveryType = deliveryType;
 	}
-	public String getIcompanyName() {
-		return icompanyName;
+	public String getDeliveryName() {
+		return deliveryName;
 	}
-	public void setIcompanyName(String icompanyName) {
-		this.icompanyName = icompanyName;
+	public void setDeliveryName(String deliveryName) {
+		this.deliveryName = deliveryName;
 	}
-	public Double getInvestmentAmount() {
-		return investmentAmount;
+	public Double getDeliveryAmount() {
+		return deliveryAmount;
 	}
-	public void setInvestmentAmount(Double investmentAmount) {
-		this.investmentAmount = investmentAmount;
+	public void setDeliveryAmount(Double deliveryAmount) {
+		this.deliveryAmount = deliveryAmount;
 	}
-	public Double getShareRatio() {
-		return shareRatio;
+	public Double getDeliveryShareRatio() {
+		return deliveryShareRatio;
 	}
-	public void setShareRatio(Double shareRatio) {
-		this.shareRatio = shareRatio;
+	public void setDeliveryShareRatio(Double deliveryShareRatio) {
+		this.deliveryShareRatio = deliveryShareRatio;
 	}
-	public String getProjectDirector() {
-		return projectDirector;
+	public int getProjectId() {
+		return projectId;
 	}
-	public void setProjectDirector(String projectDirector) {
-		this.projectDirector = projectDirector;
+	public void setProjectId(int projectId) {
+		this.projectId = projectId;
 	}
-	public String getEmceedPosition() {
-		return emceedPosition;
+	public int getCreateUid() {
+		return createUid;
 	}
-	public void setEmceedPosition(String emceedPosition) {
-		this.emceedPosition = emceedPosition;
-	}
-	public String getTelephone() {
-		return telephone;
-	}
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-	public String getAcompanyName() {
-		return acompanyName;
-	}
-	public void setAcompanyName(String acompanyName) {
-		this.acompanyName = acompanyName;
-	}
-	public Double getAinvestmentAmount() {
-		return ainvestmentAmount;
-	}
-	public void setAinvestmentAmount(Double ainvestmentAmount) {
-		this.ainvestmentAmount = ainvestmentAmount;
-	}
-	public Double getAshareRatio() {
-		return ashareRatio;
-	}
-	public void setAshareRatio(Double ashareRatio) {
-		this.ashareRatio = ashareRatio;
-	}
-	public String getAemceedPosition() {
-		return aemceedPosition;
-	}
-	public void setAemceedPosition(String aemceedPosition) {
-		this.aemceedPosition = aemceedPosition;
-	}
-	public String getAprojectDirector() {
-		return aprojectDirector;
-	}
-	public void setAprojectDirector(String aprojectDirector) {
-		this.aprojectDirector = aprojectDirector;
-	}
-	public String getAtelephone() {
-		return atelephone;
-	}
-	public void setAtelephone(String atelephone) {
-		this.atelephone = atelephone;
+	public void setCreateUid(int createUid) {
+		this.createUid = createUid;
 	}
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+	
+
 
 	
 	
