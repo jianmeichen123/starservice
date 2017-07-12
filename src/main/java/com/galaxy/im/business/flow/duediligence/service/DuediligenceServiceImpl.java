e259c9d8749cab051593869b442ec90f9fd047f3
liuli <liuli521>
2017-07-12 16:48:26 +0800

    修改尽职调查报告问题

--- src/main/java/com/galaxy/im/business/flow/duediligence/service/DuediligenceServiceImpl.java
+++ src/main/java/com/galaxy/im/business/flow/duediligence/service/DuediligenceServiceImpl.java
@@ -32,7 +32,6 @@ public class DuediligenceServiceImpl extends BaseServiceImpl<Test> implements ID
 	public Map<String, Object> duediligenceOperateStatus(Map<String, Object> paramMap) {
 		try{
 			int count1 =0;
-			int count2 =0;
 			Map<String,Object> result = new HashMap<String,Object>();
 			result.put("pass", false);
 			result.put("veto", true);
@@ -40,60 +39,38 @@ public class DuediligenceServiceImpl extends BaseServiceImpl<Test> implements ID
 			if(dataList!=null && dataList.size()>0){
 				String dictCode;
 				Integer pcount = 0;
-				String projectType="";
 				for(Map<String,Object> map : dataList){
 					dictCode = CUtils.get().object2String(map.get("dictCode"), "");
 					pcount = CUtils.get().object2Integer(map.get("pcount"), 0);
-					projectType = CUtils.get().object2String(map.get("projectType"), "");
-					if(projectType.equals("projectType:1")){
-						//投资：是否上传了:业务尽调报告、人事尽调报告、法务尽调报告、财务尽调报告、尽调启动报告、尽调总结会报告
-						if("fileWorktype:1".equals(dictCode)){
-							if(pcount>0){
-								count1++;
-							}
-						}else if("fileWorktype:2".equals(dictCode)){
-							if(pcount>0){
-								count1++;
-							}
-						}else if("fileWorktype:3".equals(dictCode)){
-							if(pcount>0){
-								count1++;
-							}
-						}else if("fileWorktype:4".equals(dictCode)){
-							if(pcount>0){
-								count1++;
-							}
-						}else if("fileWorktype:18".equals(dictCode)){
-							if(pcount>0){
-								count1++;
-							}
-						}else if("fileWorktype:19".equals(dictCode)){
-							if(pcount>0){
-								count1++;
-							}
+					
+					//是否上传了:业务尽调报告、人事尽调报告、法务尽调报告、财务尽调报告、尽调启动报告、尽调总结会报告
+					if("fileWorktype:1".equals(dictCode)){
+						if(pcount>0){
+							count1++;
 						}
-					}else{
-						//创建：
-						if("fileWorktype:1".equals(dictCode)){
-							if(pcount>0){
-								count2++;
-							}
-						}else if("fileWorktype:2".equals(dictCode)){
-							if(pcount>0){
-								count2++;
-							}
-						}else if("fileWorktype:18".equals(dictCode)){
-							if(pcount>0){
-								count2++;
-							}
-						}else if("fileWorktype:19".equals(dictCode)){
-							if(pcount>0){
-								count2++;
-							}
+					}else if("fileWorktype:2".equals(dictCode)){
+						if(pcount>0){
+							count1++;
+						}
+					}else if("fileWorktype:3".equals(dictCode)){
+						if(pcount>0){
+							count1++;
+						}
+					}else if("fileWorktype:4".equals(dictCode)){
+						if(pcount>0){
+							count1++;
+						}
+					}else if("fileWorktype:18".equals(dictCode)){
+						if(pcount>0){
+							count1++;
+						}
+					}else if("fileWorktype:19".equals(dictCode)){
+						if(pcount>0){
+							count1++;
 						}
 					}
 				}
-				if(count1>=6 || count2>=4){
+				if(count1>=6){
 					result.put("pass", true);
 				}
 			}
