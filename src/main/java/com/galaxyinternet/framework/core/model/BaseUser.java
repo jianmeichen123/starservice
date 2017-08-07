36cae0e08815abc13640268249396866bc63e2a2
liuli <liuli521>
2017-08-07 20:36:43 +0800

    添加baseuser

---
+++ src/main/java/com/galaxyinternet/framework/core/model/BaseUser.java
@@ -0,0 +1,25 @@
+package com.galaxyinternet.framework.core.model;
+
+import com.galaxyinternet.framework.core.model.PagableEntity;
+
+public class BaseUser extends PagableEntity {
+	private static final long serialVersionUID = 1L;
+
+	protected String sessionId;
+
+	/**
+	 * @return the sessionId
+	 */
+	public String getSessionId() {
+		return sessionId;
+	}
+
+	/**
+	 * @param sessionId
+	 *            the sessionId to set
+	 */
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+
+}
