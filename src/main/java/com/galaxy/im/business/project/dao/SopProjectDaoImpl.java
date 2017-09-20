c9cb4c88520a28331a95e50424ee7927d1cdf89d
liuli <liuli521>
2017-09-20 10:14:12 +0800

    项目修改问题

--- src/main/java/com/galaxy/im/business/project/dao/SopProjectDaoImpl.java
+++ src/main/java/com/galaxy/im/business/project/dao/SopProjectDaoImpl.java
@@ -1,11 +1,13 @@
 package com.galaxy.im.business.project.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
 import com.galaxy.im.bean.project.ProjectBo;
 import com.galaxy.im.bean.project.SopProjectBean;
+import com.galaxy.im.common.BeanUtils;
 import com.galaxy.im.common.CUtils;
 import com.galaxy.im.common.db.BaseDaoImpl;
 import com.galaxy.im.common.db.Page;
@@ -60,8 +62,9 @@ public class SopProjectDaoImpl extends BaseDaoImpl<ProjectBo,Long> implements IS
 		List<SopProjectBean> contentList = null;
 		long total = 0L;
 		try {
+			Map<String, Object> params = BeanUtils.toMap(query);
 			contentList = sqlSessionTemplate.selectList( getSqlName("selectPageList"),getParams(query, pageable));
-			total = CUtils.get().object2Long(sqlSessionTemplate.selectOne(getSqlName("selectPageListCount"),getParams(query, pageable)));		
+			total = CUtils.get().object2Long(sqlSessionTemplate.selectOne(getSqlName("selectPageListCount"),params));		
 			return new  Page<SopProjectBean>(contentList, pageable, total); 
 		} catch (Exception e) {
 			throw new DaoException(String.format("根据分页对象查询列表出错！语句:%s", getSqlName("selectPageList")), e);
