675d1da56a3bd75c5698facf02efde72a0e27b38
liuli <liuli521>
2017-12-11 11:25:49 +0800

    修改项目承揽人搜索功能

--- src/main/java/com/galaxy/im/bean/project/SopProjectBean.java
+++ src/main/java/com/galaxy/im/bean/project/SopProjectBean.java
@@ -46,8 +46,8 @@ public class SopProjectBean extends PagableEntity {
     private String nextFinancingSource;			//下一轮融资路径
     private String industryAnalysis;			//行业分析
     private String operationalData;				//运营数据
-    private String faFlag;						//项目来源
-    private String faName;						//FA全名
+    //private String faFlag;						//项目来源
+    //private String faName;						//FA全名
     private String greanChannel;				//项目绿色通道标识(多通道支持,分割) 1：接触访谈，2：内部评审，3：CEO评审，4：立项会，5：投资意向书，6：尽职调查，7：投资决策会，8：投资协议，9：股权交割
     private String remark;						//备注
     private Long projectTime; 					//项目创建时间（数据库保存时间）
@@ -78,6 +78,10 @@ public class SopProjectBean extends PagableEntity {
         
     private String projectStatusName;  //项目状态名称;
     
+    
+    private String projectSource;			//项目来源
+    private String projectSourceName;		//项目来源对应的填写项名称
+    private String projectUsers;			//项目承揽人(id)
     
     
     
@@ -312,18 +316,6 @@ public class SopProjectBean extends PagableEntity {
 		this.operationalData = operationalData;
 	}
 	
-	public String getFaFlag() {
-		return faFlag;
-	}
-	public void setFaFlag(String faFlag) {
-		this.faFlag = faFlag;
-	}
-	public String getFaName() {
-		return faName;
-	}
-	public void setFaName(String faName) {
-		this.faName = faName;
-	}
 	public String getGreanChannel() {
 		return greanChannel;
 	}
@@ -461,6 +453,24 @@ public class SopProjectBean extends PagableEntity {
 	}
 	public void setProjectStatusName(String projectStatusName) {
 		this.projectStatusName = projectStatusName;
+	}
+	public String getProjectSource() {
+		return projectSource;
+	}
+	public void setProjectSource(String projectSource) {
+		this.projectSource = projectSource;
+	}
+	public String getProjectUsers() {
+		return projectUsers;
+	}
+	public void setProjectUsers(String projectUsers) {
+		this.projectUsers = projectUsers;
+	}
+	public String getProjectSourceName() {
+		return projectSourceName;
+	}
+	public void setProjectSourceName(String projectSourceName) {
+		this.projectSourceName = projectSourceName;
 	}
 	
 }
