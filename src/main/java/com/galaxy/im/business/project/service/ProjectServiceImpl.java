6c086ccaf6b59d91c519ec81ab90b834bd59371a
liuli <liuli521>
2017-12-13 14:26:01 +0800

    11

--- src/main/java/com/galaxy/im/business/project/service/ProjectServiceImpl.java
+++ src/main/java/com/galaxy/im/business/project/service/ProjectServiceImpl.java
@@ -262,7 +262,7 @@ public class ProjectServiceImpl extends BaseServiceImpl<ProjectBean> implements 
 			Map<String,Object> map1 = dao.selectBaseProjectInfo(paramMap);
 			//项目来源
 			Map<String,Object> sourceMap = dao.selectProjectSoureInfo(paramMap);
-			if (CUtils.get().object2Integer(sourceMap.get("projectSoureId")) != 2262) {
+			if (!sourceMap.isEmpty() && sourceMap.containsKey("projectSoureId") && CUtils.get().object2Integer(sourceMap.get("projectSoureId")) != 2262) {
 				String tempId = "";
 				projectMap.put("inputId", sourceMap.get("projectSoureId"));
 				tempId = dao.findInputTitleId(projectMap);
