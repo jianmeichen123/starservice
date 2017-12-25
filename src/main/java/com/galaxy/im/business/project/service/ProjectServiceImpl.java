9353956a20d77f1204f6826df9d207c634451f40
liuli <liuli521>
2017-12-25 16:50:47 +0800

    修改项目详情问题

--- src/main/java/com/galaxy/im/business/project/service/ProjectServiceImpl.java
+++ src/main/java/com/galaxy/im/business/project/service/ProjectServiceImpl.java
@@ -293,10 +293,10 @@ public class ProjectServiceImpl extends BaseServiceImpl<ProjectBean> implements 
 						StringBuilder sBuilder = new StringBuilder();
 						StringBuilder sb = new StringBuilder();
 						for(Map<String,Object> maps : map3){
-							if (maps.get("projectUserName")!=null && maps.get("projectUser")!=null) {
+							if (maps.containsKey("projectUserName") && maps.get("projectUserName")!=null) {
 								projectMap.put("otherProjectUser", CUtils.get().object2String(maps.get("projectUserName")));
 							}
-							if (maps.get("projectUser")!=null) {
+							if (maps.containsKey("projectUser") && maps.get("projectUser")!=null) {
 								if (maps.get("projectUser")!=null && maps.get("projectUserId").equals("10072")) {
 									sBuilder.append(projectMap.get("otherProjectUser") + "、");
 								}else {
