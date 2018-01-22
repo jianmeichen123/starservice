439c956271bbb5969b9f64cd0873a88a8ce9ded8
liuli <liuli521>
2018-01-22 10:28:03 +0800

    新增创意列表接口（待完善）

--- src/main/java/com/galaxy/im/business/idea/controller/IdeaController.java
+++ src/main/java/com/galaxy/im/business/idea/controller/IdeaController.java
@@ -1,13 +1,38 @@
 package com.galaxy.im.business.idea.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.galaxy.im.business.idea.service.IIdeaService;
+import com.galaxy.im.common.ResultBean;
 
 @Controller
+@RequestMapping("/idea")
 public class IdeaController {
+	private Logger log = LoggerFactory.getLogger(IdeaController.class);
 
 	@Autowired
 	IIdeaService service;
+	
+	/**
+	 * 创意列表，搜索
+	 * @param paramString
+	 * @return
+	 */
+	@RequestMapping("search")
+	@ResponseBody
+	public Object search(@RequestBody String paramString){
+		ResultBean<Object> resultBean = new ResultBean<Object>();
+		try{
+			
+		}catch(Exception e){
+			log.error(IdeaController.class.getName() + "search",e);
+		}
+		return resultBean;
+	}
 }
