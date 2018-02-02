a9493c210371b5697f478505a0df0efdb9cae6f1
liuli <liuli521>
2018-02-02 11:59:11 +0800

    修改承揽人

--- src/main/java/com/galaxy/im/business/project/service/ProjectServiceImpl.java
+++ src/main/java/com/galaxy/im/business/project/service/ProjectServiceImpl.java
@@ -315,19 +315,29 @@ public class ProjectServiceImpl extends BaseServiceImpl<ProjectBean> implements 
 						StringBuilder sb = new StringBuilder();
 						for(Map<String,Object> maps : map3){
 							if(maps!=null && !maps.isEmpty()){
-								if (maps.get("projectUserName")!=null || maps.get("projectUser")!=null) {
+								/*if (maps.get("projectUserName")!=null && maps.get("projectUser")!=null) {
 									projectMap.put("otherProjectUser", CUtils.get().object2String(maps.get("projectUserName")));
 								}
 								if (maps.get("projectUser")!=null) {
-									if (maps.get("projectUser")!=null || maps.get("projectUserId").equals("10072")) {
+									if (maps.get("projectUser")!=null && maps.get("projectUserId").equals("10072")) {
 										sBuilder.append(projectMap.get("otherProjectUser") + "、");
 									}else {
 										sBuilder.append(maps.get("projectUser") + "、");
 									}
+								}*/
+								if(maps.get("projectUserId").equals("10072") && maps.get("projectUserName")!=null){
+									projectMap.put("otherProjectUser", CUtils.get().object2String(maps.get("projectUserName")));
 								}
+								
+								if (maps.get("projectUser")!=null && maps.get("projectUserName")==null) {
+									sBuilder.append(maps.get("projectUser") + "、");
+								}
+								
 								if (maps.get("projectUser")==null && maps.get("projectUserName")!=null) {
 									sBuilder.append(maps.get("projectUserName") + "、");
 								}
+								
+								
 								if(maps.get("projectUserId")!=null){
 									sb.append(maps.get("projectUserId") + "、");
 								}
